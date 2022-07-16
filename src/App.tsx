151718e85267f61ef7790eb7cbff42ce829b25ce
serega-s <comp.it.05@inbox.ru>
2022-07-16 13:24:56 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react"
+import { Route, Routes } from "react-router-dom"
+import Navigation from "./components/Navigation"
+import FavouritesPage from "./pages/FavouritesPage"
+import HomePage from "./pages/HomePage"
+
+function App() {
+  return (
+    <>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/favourites" element={<FavouritesPage />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
