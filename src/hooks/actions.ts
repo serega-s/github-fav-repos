d6fc071bd7a2fc67e1e5241c0a647c4e6e0c7340
serega-s <comp.it.05@inbox.ru>
2022-07-16 13:21:25 +0300

    actions

---
+++ src/hooks/actions.ts
@@ -0,0 +1,13 @@
+import { bindActionCreators } from "@reduxjs/toolkit"
+import { useDispatch } from "react-redux"
+import { githubActions } from "../store/github/github.slice"
+
+const actions = {
+  ...githubActions,
+}
+
+export const useActions = () => {
+  const dispatch = useDispatch()
+
+  return bindActionCreators(actions, dispatch)
+}
